438d59b2e7e73351fab2d1f502c277a815f95588
Ras Goal Gule <abelberhau21@gmail.com>
2017-03-05 17:43:39 -0700

    inital
    project implementation inital stage from the existing project

---
+++ master/footer.php
@@ -0,0 +1,28 @@
+
+<style>
+    footer {
+        background-color: #555;
+        color: white;
+        padding: 15px;
+    }
+
+</style>
+<!--page on should have one body tag-->
+<div id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <footer class="container-fluid text-center" id="fo" >
+        
+        <a class="up-arrow" href="#myPage"  title="TO TOP">    
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+        
+        
+  <br>
+        
+        <?php
+        echo "<p>Copyright &copy; 2017-" . date("Y") . " einfo.com</p>";
+        ?>
+    </footer>
+</div>
+</body>
+</html>
+<?php  include $_SERVER['DOCUMENT_ROOT'] . '/ILSP-group-final-project/pages/login.php'; ?>
---
+++ pages/element/login.php
@@ -0,0 +1,32 @@
+<?php
+         ?>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Log In</h4>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label for="usr">User Name:</label>
+          <input type="text" class="form-control" id="usr">
+        </div>
+        <div class="form-group">
+          <label for="pwd">Password:</label>
+          <input type="password" class="form-control" id="pwd">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" >Log In</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
